9c7a16489aeb23409c3566330e781fc866fa4e4f
Florian Wiesner <wiesnerflorian94@gmail.com>
2024-06-30 12:21:41 +0200

    first try to impl convolution (1D)

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Custom CUDA kernel to accelerate masked 3D convolutions.
 
 Currently used to learn CUDA, hence the code is not production-ready and might not even be correct yet. Please use at your own risk.
+
+
+Notes:
+- Thrust lib is awesome to use std-like features (vector, unique_ptr etc)
--- main.cu
+++ main.cu
@@ -55,43 +55,85 @@ __global__ void dot_product(float* out, float* v1, float* v2, int n) {
     }
 }
 
+__global__ void convolution_1d(float* out, float* arr, float* kernel, int n_arr, int n_kernel, bool* mask, float pad_val) {
+
+    int t_idx = threadIdx.x; // thread index
+    int b_dim = blockDim.x; // number of threads per block
+    int b_idx = blockIdx.x; // block index
+
+    int idx = b_idx * b_dim + t_idx;
+    int stride = b_dim * gridDim.x; // total number of threads
+
+    for (int i = idx; i < n_arr; i += stride) {
+        out[i] = 0.0f;
+
+        // loop over kernel
+        for (int j = 0; j < n_kernel; ++j) {
+            if (i - j >= 0 && i - j < n_arr) {
+                out[i] += arr[i - j] * kernel[j];
+            }
+            else {
+                out[i] += pad_val * kernel[j];
+            }
+        }
+    }
+
+
+}
+
 int main() {
-    int N = 10000000;
+    int vector_size = 10000000;
+    int kernel_size = 100;
+
+
+    // TODO: use deviceQuery to get best block size
     int block_size = 256;
     // rounded up to nearest multiple of block size
-    int num_blocks = (N + block_size - 1) / block_size;
+    int num_blocks = (vector_size + block_size - 1) / block_size;
 
     // Declare variables
     float* v1;
-    float* v2;
-    float* out;
+    float* kernel;
+    float* v_out;
+    bool* mask;
 
     // Allocate memory
-    cudaMallocManaged(&v1, N * sizeof(float));
-    cudaMallocManaged(&v2, N * sizeof(float));
-    cudaMallocManaged(&out, N * sizeof(float));
+    cudaMallocManaged(&v1, vector_size * sizeof(float));
+    cudaMallocManaged(&kernel, vector_size * sizeof(float));
+    cudaMallocManaged(&v_out, vector_size * sizeof(float));
+    cudaMallocManaged(&mask, kernel_size * sizeof(bool));
 
     // Initialize variables
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < vector_size; i++) {
         v1[i] = 1.0f;
-        v2[i] = 2.0f;
+    }
+
+    for (int i = 0; i < kernel_size; i++) {
+        kernel[i] = 1.0f;
+        if (i % 2 == 0) {
+            mask[i] = true;
+        }
+        else {
+            mask[i] = false;
+        }
     }
 
     // Launch kernel
-    dot_product << < num_blocks, block_size >> > (out, v1, v2, N);
+    convolution_1d << < num_blocks, block_size, block_size * sizeof(float) >> > (v_out, v1, kernel, vector_size, kernel_size, mask, 0.0f);
 
     // Wait for kernel to finish
     cudaDeviceSynchronize();
 
     // Print result
     // should be 2 * N
-    std::cout << *out << std::endl;
+    std::cout << v_out[50] << std::endl;
 
 
     // Free memory
     cudaFree(v1);
-    cudaFree(v2);
-    cudaFree(out);
+    cudaFree(kernel);
+    cudaFree(v_out);
+    cudaFree(mask);
 
     return 0;
 }
