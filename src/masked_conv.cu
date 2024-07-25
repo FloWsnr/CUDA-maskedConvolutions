9672902a1ce18644018f84eca829284354fe9dec
Florian Wiesner <wiesnerflorian94@gmail.com>
2024-07-25 21:30:33 +0200

    start more indepth tests for conv3D

--- src/masked_conv.cu
+++ src/masked_conv.cu
@@ -86,12 +86,12 @@ __global__ void convolution_3d_kernel(
     int n_arr = nx_arr * ny_arr * nz_arr;
     int n_kernel = nx_kernel * ny_kernel * nz_kernel;
 
-    unsigned int t_idx{ threadIdx.x }; // thread index
-    unsigned int b_dim{ blockDim.x }; // number of threads per block
-    unsigned int b_idx{ blockIdx.x }; // block index
+    const unsigned int t_idx{ threadIdx.x }; // thread index
+    const unsigned int b_dim{ blockDim.x }; // number of threads per block
+    const unsigned int b_idx{ blockIdx.x }; // block index
 
-    unsigned int idx{ b_idx * b_dim + t_idx };
-    unsigned int stride{ b_dim * gridDim.x }; // total number of threads
+    const unsigned int idx{ b_idx * b_dim + t_idx };
+    const unsigned int stride{ b_dim * gridDim.x }; // total number of threads
 
     for (int i = idx; i < n_arr; i += stride) {
         if (!mask[i]) continue; // skip if mask is false
