db31aa3c297d645bde5e97711fbbdbc1fab5194c
Florian Wiesner <wiesnerflorian94@gmail.com>
2024-07-26 21:00:04 +0200

    first step to repair 3d conv, not working yet

--- src/masked_conv.cu
+++ src/masked_conv.cu
@@ -82,10 +82,6 @@ __global__ void convolution_3d_kernel(
     const float pad_val) {
 
     // TODO: use shared memory for the parameters
-
-    int n_arr = nx_arr * ny_arr * nz_arr;
-    int n_kernel = nx_kernel * ny_kernel * nz_kernel;
-
     const unsigned int t_idx{ threadIdx.x }; // thread index
     const unsigned int b_dim{ blockDim.x }; // number of threads per block
     const unsigned int b_idx{ blockIdx.x }; // block index
@@ -93,15 +89,35 @@ __global__ void convolution_3d_kernel(
     const unsigned int idx{ b_idx * b_dim + t_idx };
     const unsigned int stride{ b_dim * gridDim.x }; // total number of threads
 
-    for (int i = idx; i < n_arr; i += stride) {
+    const int n_arr = nx_arr * ny_arr * nz_arr;
+    const int n_kernel = nx_kernel * ny_kernel * nz_kernel;
+
+
+    int i, j, x, y, z;
+    int xShift, yShift, zShift;
+    int input_index, input_index_x, input_index_y, input_index_z;
+    const int kernelCenterX = nx_kernel / 2;
+    const int kernelCenterY = ny_kernel / 2;
+    const int kernelCenterZ = nz_kernel / 2;
+
+    for (i = idx; i < n_arr; i += stride) {
         if (!mask[i]) continue; // skip if mask is false
 
         // loop over kernel
-        for (int x = 0; x < nx_kernel; ++x) {
-            for (int y = 0; y < ny_kernel; ++y) {
-                for (int z = 0; z < nz_kernel; ++z) {
-                    int j = x * ny_kernel * nz_kernel + y * nz_kernel + z;
-                    int input_index = i - j + n_kernel / 2;
+        for (x = 0; x < nx_kernel; ++x) {
+            for (y = 0; y < ny_kernel; ++y) {
+                for (z = 0; z < nz_kernel; ++z) {
+
+                    xShift = x - kernelCenterX;
+                    yShift = y - kernelCenterY;
+                    zShift = z - kernelCenterZ;
+
+                    input_index_x = (x - xShift) / 2;
+                    input_index_y = y - yShift / 2;
+                    input_index_z = z - zShift / 2;
+
+                    j = input_index_x * ny_kernel * nz_kernel + input_index_y * nz_kernel + input_index_z;
+                    input_index = i - j + n_kernel / 2;
                     if (!mask[input_index]) continue; // skip if mask is false
                     if (input_index >= 0 && input_index < n_arr) {
                         out[i] += arr[input_index] * kernel[j];
